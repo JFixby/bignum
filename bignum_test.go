f0e237cab554b6ced0f2fd3d8ae7ba2c67d4a0ca
J Fixby <github@jfixby.com>
2019-08-15 19:53:46 +0200

    stash

--- bignum_test.go
+++ bignum_test.go
@@ -2,36 +2,46 @@ package bignum
 
 import (
 	"fmt"
-	"github.com/jfixby/pin"
 	"testing"
 )
 
 func TestCalcSubsidy(t *testing.T) {
+	subsidyBlocksNumber := int64(16)
+	targetTotalSubsidy := float64(1)
 	Engine = Float64Engine{}
-	runTest()
+	resultFloat64 := testCalcSubsidy(subsidyBlocksNumber, targetTotalSubsidy, 1).ToFloat64()
 	Engine = BigIntEngine{}
-	runTest()
+	resultBigFloat := testCalcSubsidy(subsidyBlocksNumber, targetTotalSubsidy, 1).ToFloat64()
+
+	if resultFloat64 != (resultBigFloat) {
+		t.Fatalf("mismatched total subsidy -- \n got %v, \nwant %v", resultFloat64, resultBigFloat)
+	}
 }
 
-func runTest() {
-	subsidyBlocksNumber := int64(3)
-	T := float64(7)
+func testCalcSubsidy(subsidyBlocksNumber int64, targetTotalSubsidy float64, printIterations int64) BigNum {
 	testHeight := subsidyBlocksNumber
 	totalSubsidy := NewBigNum(0)
 	for blockNum := int64(0); blockNum <= testHeight; blockNum++ {
-		sub := calcSubsidy(subsidyBlocksNumber, blockNum, T)
+		sub := calcSubsidy(subsidyBlocksNumber, blockNum, targetTotalSubsidy)
 		//totalSubsidy += sub
 		totalSubsidy = totalSubsidy.Add(totalSubsidy, sub)
-		pin.D(fmt.Sprintf("%v", blockNum), sub)
+		if blockNum%printIterations == 0 {
+			blockNumPad := fmt.Sprintf("%2v", blockNum)
+			subPad := fmt.Sprintf("%-20v", sub.ToFloat64())
+			totalSubsidyPad := fmt.Sprintf("%-20v", totalSubsidy.ToFloat64())
+			fmt.Println(fmt.Sprintf("[%v] %v coins %v total", blockNumPad, subPad, totalSubsidyPad))
+		}
 	}
-	pin.D(fmt.Sprintf("totalSubsidy"), totalSubsidy.ToFloat64())
+	fmt.Println(fmt.Sprintf("totalSubsidy: %16v", totalSubsidy.ToFloat64()))
+	fmt.Println("")
+	return totalSubsidy
 }
 
 func calcSubsidy(subsidyBlocksNumber int64, height int64, totalSubsidy float64) BigNum {
 	if height == 0 { //genesis block
-		//return 0
+		return NewBigNum(0)
 	}
-	H := NewBigNum(height)
+	H := NewBigNum(height - 1)
 	N := NewBigNum(subsidyBlocksNumber)
 
 	//lastBlockIndex := new(big.Int).SetInt64(subsidyBlocksNumber - 1)
@@ -44,7 +54,7 @@ func calcSubsidy(subsidyBlocksNumber int64, height int64, totalSubsidy float64) 
 	//return totalSubsidy * 2.0 * (lastBlockIndex - H) / (N * lastBlockIndex)
 	H = H.Neg(H)                 // -H
 	H = H.Add(lastBlockIndex, H) // (lastBlockIndex - H)
-	H = H.Mul(NewBigNum(2), H)          // 2.0 * (lastBlockIndex - H)
+	H = H.Mul(NewBigNum(2), H)   // 2.0 * (lastBlockIndex - H)
 	N = N.Mul(N, lastBlockIndex) // (N * lastBlockIndex)
 
 	//subsidy := big.NewRat(1, 1)
